f1f4264ff55ac6b2e25e70a254ff3c8c1b47ece4
Pitchuka-kamalesh <kamalesh.4025@gmail.com>
2024-03-15 16:07:04 +0530

    implementyed the total APi reporting using the Extents Reports

--- src/main/java/com/utills/ExtentReportManager.java
+++ src/main/java/com/utills/ExtentReportManager.java
@@ -1,17 +1,22 @@
 package com.utills;
 
 import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.markuputils.CodeLanguage;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
+import io.restassured.http.Header;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public class ExtentReportManager {
 
     public static ExtentReports extentReports;
 
-    public static ExtentReports createInstance(String fileName,String reportName,String documentTitle){
+    public static ExtentReports createInstance(String fileName, String reportName, String documentTitle) {
         ExtentSparkReporter extentSparkReporter = new ExtentSparkReporter(fileName);
         extentSparkReporter.config().setTheme(Theme.DARK);
         extentSparkReporter.config().setReportName(reportName);
@@ -24,11 +29,52 @@ public class ExtentReportManager {
 
     }
 
-    public static String getReportNameWithTimeStamp(){
+    public static String getReportNameWithTimeStamp() {
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy_MM_dd_HH_mm_ss");
         LocalDateTime localDateTime = LocalDateTime.now();
         String datetime = dateTimeFormatter.format(localDateTime);
-        return "reports/extent/"+datetime+".html";
+        return "reports/extent/" + datetime + ".html";
+    }
+
+    public static void logPassDetails(String log) {
+        Setup.extentTest.get().pass(MarkupHelper.createLabel(log, ExtentColor.GREEN));
+    }
+
+    public static void logFailDetails(String log) {
+        Setup.extentTest.get().fail(MarkupHelper.createLabel(log, ExtentColor.RED));
+    }
+
+    public static void logStacktrace(String log) {
+        String trace = log.replaceAll(",", "<br>");
+        String finalStracktrace = "<details>/n" +
+                "<summary>Click here to view the Exception stackTrace</summary>/n" +
+                trace + "</details>/n";
+        Setup.extentTest.get().fail(finalStracktrace);
+    }
+
+    public static void logSkippedDetails(String log) {
+        Setup.extentTest.get().skip(MarkupHelper.createLabel(log, ExtentColor.YELLOW));
+    }
+
+    public static void logWarningDetails(String log) {
+        Setup.extentTest.get().warning(MarkupHelper.createLabel(log, ExtentColor.BLUE));
+    }
+
+    public static void logInfoDetails(String log) {
+        Setup.extentTest.get().info(MarkupHelper.createLabel(log, ExtentColor.ORANGE));
+    }
+    public static void logJsonBody(String log){
+        Setup.extentTest.get().info(MarkupHelper.createCodeBlock(log, CodeLanguage.JSON));
+    }
+
+    public static void logXMLBody(String json){
+        Setup.extentTest.get().info(MarkupHelper.createCodeBlock(json,CodeLanguage.XML));
+    }
+
+    public static  void  logHeaders(List<Header> headersList){
+        String[][] arrayHeaders = headersList.stream().map(header -> new String[] {header.getName(), header.getValue()}).toArray(String[][] :: new);
+        Setup.extentTest.get().info(MarkupHelper.createTable(arrayHeaders));
+
     }
 
 }
--- src/main/java/com/utills/Setup.java
+++ src/main/java/com/utills/Setup.java
@@ -1,15 +1,19 @@
 package com.utills;
 
 import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import java.util.Arrays;
+
 public class Setup implements ITestListener {
     private static ExtentReports extentReport;
+    public static ThreadLocal<ExtentTest> extentTest = new ThreadLocal<>();
     @Override
     public void onStart(ITestContext context) {
-        extentReport =  ExtentReportManager.createInstance(ExtentReportManager.getReportNameWithTimeStamp(),"TestApiAutomaction","Api Testing")
+        extentReport =  ExtentReportManager.createInstance(ExtentReportManager.getReportNameWithTimeStamp(),"TestApiAutomaction","Api Testing");
 
     }
 
@@ -19,4 +23,27 @@ public class Setup implements ITestListener {
             extentReport.flush();
         }
     }
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ExtentTest test = extentReport.createTest("TestName" +result.getTestClass().getTestName()+" - "+result.getMethod().getMethodName());
+        extentTest.set(test);
+    }
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        // not implemented
+    }
+    @Override
+    public void onTestFailure(ITestResult result) {
+        ExtentReportManager.logFailDetails(result.getThrowable().getMessage());
+        ExtentReportManager.logStacktrace(Arrays.toString(result.getThrowable().getStackTrace()));
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        // not implemented
+    }
+
+
+
 }
